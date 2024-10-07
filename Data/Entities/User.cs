439b39debbcfc1e24c7cce6da1b9dcc7ab6118e7
mauricio tempone <143232326+mauritempo@users.noreply.github.com>
2024-10-07 19:37:41 -0300

    Entidad User, creacion de tabla en base de datos.

--- Data/Entities/User.cs
+++ Data/Entities/User.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +10,8 @@ namespace Data.Entities
 {
     public class User
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
         // Nombre de usuario, requerido y único
