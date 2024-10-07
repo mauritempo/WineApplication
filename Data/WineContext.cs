d30dd111e6502b1888dc3b12877b346cd394e866
mauricio tempone <143232326+mauritempo@users.noreply.github.com>
2024-10-07 19:42:25 -0300

    Creacion del contexto, lo uso para comunicar DB.

---
+++ Data/WineContext.cs
@@ -0,0 +1,17 @@
+﻿using Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class WineContext: DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Wine> Wines { get; set; }
+        public WineContext(DbContextOptions<WineContext> options) : base(options) { }
+    }
+}
