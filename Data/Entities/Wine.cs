e3a40bf523db9433bcaffa1b596b350507fd53a6
mauricio tempone <143232326+mauritempo@users.noreply.github.com>
2024-10-07 19:37:58 -0300

    Entidad Wine, creacion de tabla en base de datos.

--- Data/Entities/Wine.cs
+++ Data/Entities/Wine.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,8 @@ namespace Data.Entities
 {
     public class Wine
     {
-
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
         // El nombre del vino, requerido
