2b43b135d245812174c14383a2941a1aedc98a2a
mauricio tempone <143232326+mauritempo@users.noreply.github.com>
2024-10-07 19:48:02 -0300

    Cambio las inyecciones de dependecias a scoped para podes interactuar con la DB. asigno la creacion del archivo .DB

--- WineAppliaction Git/Program.cs
+++ WineAppliaction Git/Program.cs
@@ -1,5 +1,8 @@
+using Data;
 using Data.Repo;
 using Service;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,10 +12,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddSingleton<UserRepository>();
-builder.Services.AddSingleton<WineRepository>();
+builder.Services.AddScoped<UserRepository>();
+builder.Services.AddScoped<WineRepository>();
 builder.Services.AddScoped<IUserServices, UserServices>();
 builder.Services.AddScoped<IWineService, WineServices>();
+builder.Services.AddDbContext<WineContext>(dbContextOptions => dbContextOptions.UseSqlite(
+builder.Configuration["ConnectionStrings:DBConnectionString"]));
+
 
 
 var app = builder.Build();
